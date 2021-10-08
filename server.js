a40e8e068609c8c1fd1cba30fb513b9a56670bc6
Chris Bradford <87334834+bradford583@users.noreply.github.com>
2021-10-08 13:42:28 -0600

    added server routes and dependencies

---
+++ server.js
@@ -0,0 +1,22 @@
+//node modules
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+
+//set port
+const PORT = process.env.PORT || 3001;
+
+//routes
+const apiRoutes = require('./routes/apiRoutes');
+const hmtlRoutes = require('./routes/htmlRoutes');
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
