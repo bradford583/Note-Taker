5d4488f837ad96a9e12c0ee84c813f71c81eb569
Chris Bradford <87334834+bradford583@users.noreply.github.com>
2021-10-14 18:47:40 -0600

    really finished

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 
-router.get("/api/notes", (req, res) => {
+router.get("/notes", (req, res) => {
     //get notes on page
     fs.readFile(path.join(__dirname, '../../db/db.json'), 'utf8', (err,data) => {
         if (err) throw err;
@@ -12,7 +12,7 @@ router.get("/api/notes", (req, res) => {
     });
 });
 
-router.post('/api/notes', (req, res) => {
+router.post('/notes', (req, res) => {
     const newNote = req.body;
     newNote.id = uuidv4()
 
@@ -30,14 +30,14 @@ router.post('/api/notes', (req, res) => {
     });
 });
 
-router.delete('/api/notes/:id', (req, res) => {
+router.delete('/notes/:id', (req, res) => {
     const id = req.params.id;
 
     return fs.readFile(path.join(__dirname, '../../db/db.json'), 'utf8', (err, data) => {
         if (err) throw err;
         const allNotes = JSON.parse(data);
 
-        const filteredNotes = allNotes.filter((note) => id !==noteid);
+        const filteredNotes = allNotes.filter((note) => id !==note.id);
 
         fs.writeFile(path.join(__dirname, '../../db/db.json'), JSON.stringify(filteredNotes), 'utf8', () => {
             res.status(200).json({ success:true });
